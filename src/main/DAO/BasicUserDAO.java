07d7e79ac60270e536589c419a1221be0a6b609b
CASician <cristian.sician@gmail.com>
2024-03-11 11:13:41 +0100

    style: add comments and cli respond to Database interaction

--- src/main/DAO/BasicUserDAO.java
+++ src/main/DAO/BasicUserDAO.java
@@ -11,15 +11,22 @@ import main.DAO.DBconnection;
 
 public class BasicUserDAO {
     public static void add (BasicUser basicUser) throws SQLException {
+        // Connect to Database
         Connection connection = DriverManager.getConnection(DBconnection.jdbcUrl+"?user="+DBconnection.username+"&password="+DBconnection.password);
         PreparedStatement insertBasicUser = connection.prepareStatement("INSERT INTO BasicUsers(id, email, city, username) VALUES(DEFAULT, ?, ?, ?)");
+
+        // Add the real values instead of "?"
         insertBasicUser.setString(1, basicUser.getEmail());
         insertBasicUser.setString(2, basicUser.getCity());
         insertBasicUser.setString(3, basicUser.getUsername());
+
+        //Close connection
         insertBasicUser.executeUpdate();
         insertBasicUser.close();
         connection.close();
-        System.out.println("New BASIC USER added successfully");
+
+        //Show result
+        System.out.println("New BASIC USER added successfully!");
     }
 
     //TODO: Implementare tutte le altre funzioni
