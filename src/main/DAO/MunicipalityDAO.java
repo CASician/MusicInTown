3f01bff66c7f5e24c99f4918fb761213fca0fef3
CASician <cristian.sician@gmail.com>
2024-03-11 11:08:43 +0100

    chore: update MunicipalityDAO with proper server connection

--- src/main/DAO/MunicipalityDAO.java
+++ src/main/DAO/MunicipalityDAO.java
@@ -5,11 +5,19 @@ import java.sql.*;
 public class MunicipalityDAO {
 
     public void add (Municipality municipality)  throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:postgresql:"+"MusicInTown.db");
+        // Connect to Database
+        Connection conn = DriverManager.getConnection(DBconnection.jdbcUrl+"?user="+DBconnection.username+"&password="+DBconnection.password);
         PreparedStatement insertMunicipality = conn.prepareStatement("INSERT INTO Municipalities(id) VALUES(DEFAULT)");
+
+        //No addition needed: the id is calculated automatically
+
+        //Close connection
         insertMunicipality.executeUpdate();
         insertMunicipality.close();
         conn.close();
+
+        // Show results
+        System.out.println("New MUNICIPALITY added successfully!");
     }
 
     //TODO: implement other functions: update, delete, get(id), getAll
